a3cfed824421937ea5ab4e182c23ded7c9a393a4
Mahred1 <mredwan.kemal@gmail.com>
2026-05-02 02:14:00 +0300

    Solved String literals

---
+++ codebase/Mahfuz Redwan/String_literal.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+contract Contract {
+    bytes32 public msg1 = "Hello World";
+    string public msg2 = "This string is specifically designed to be much longer than thirty-two bytes to meet the requirement.";
+}
